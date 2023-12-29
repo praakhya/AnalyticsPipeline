35349419ccc4fdaa3644f2cdd2bc10262261334b
praakhya <67508200+praakhya@users.noreply.github.com>
2023-12-29 21:07:10 +0530

    tried d3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { WorkspaceComponent } from './workspace/workspace.component';
 
 export const routes: Routes = [
     {path:"", component:HomeComponent},
     {path:"login", component:LoginComponent},
     {path:"signup",component:SignupComponent},
-    {path:"dashboard", component:DashboardComponent}
+    {path:"dashboard", component:DashboardComponent},
+    {path:"testing", component:WorkspaceComponent}
 ];
