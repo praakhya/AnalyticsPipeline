3768f0da78ae92e4e5aab8f9d82c467c82c14c0f
praakhya <67508200+praakhya@users.noreply.github.com>
2024-01-26 11:56:03 +0530

    first landing page complete

--- analytics_pipeline_wireframe/src/landing/landing.component.ts
+++ analytics_pipeline_wireframe/src/landing/landing.component.ts
@@ -1,17 +1,36 @@
 import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-
+import { FooterComponent } from '../app/footer/footer.component';
 @Component({
   selector: 'app-landing',
   standalone: true,
   imports: [
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    FooterComponent
   ],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
 export class LandingComponent {
-  
+  appName:string = "Analytics Pipeline"
+  dataSources:any = [
+    {
+      name: "MongoDB",
+      url: "./assets/logos/mongodb.png"
+    },
+    {
+      name: "MySQL",
+      url: "./assets/logos/mysql.png"
+    },
+    {
+      name: "Neo4j",
+      url: "./assets/logos/neo4j.png"
+    },
+    {
+      name: "PostgreSQL",
+      url: "./assets/logos/postgresql.png"
+    }
+  ]
 }
